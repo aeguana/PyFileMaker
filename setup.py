34b16c0552376a02730ed8159d2d961719bce694
Bob Tanner <tanner@real-time.com>
2015-08-21 15:00:56 -0500

    Added some pypi classifiers. Added keywords. Changed the url. Added the download_url.

--- setup.py
+++ setup.py
@@ -3,14 +3,28 @@
 from setuptools import setup
 from PyFileMaker import __version__, __doc__
 
-setup(name='PyFileMaker',
-      version=__version__,
-      description='Python Object Wrapper for FileMaker Server XML Interface',
-      long_description=__doc__,
-      author='Klokan Petr Pridal, Pieter Claerhout, Marcin Kawa',
-      author_email='klokan@klokan.cz, pieter@yellowduck.be, kawa.macin@gmail.com',
-      license='http://www.opensource.org/licenses/bsd-license.php',
-      url='http://code.google.com/p/pyfilemaker/',
-      platforms = ["any"],
-      packages=['PyFileMaker']
-     )
+setup(
+    name='PyFileMaker',
+    version=__version__,
+    description='Python Object Wrapper for FileMaker Server XML Interface',
+    long_description=__doc__,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Database :: Database Engines/Servers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    keywords='FileMaker'
+    author='Klokan Petr Pridal, Pieter Claerhout, Marcin Kawa',
+    author_email='klokan@klokan.cz, pieter@yellowduck.be, kawa.macin@gmail.com',
+    url='https://github.com/aeguana/PyFileMaker',
+    download_url='https://github.com/aeguana/PyFileMaker/releases'
+    license='http://www.opensource.org/licenses/bsd-license.php',
+    platforms = ['any'],
+    packages=['PyFileMaker']
+)
